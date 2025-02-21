e68b7355b2eacd5697b6ef3367d9506637cad20e
nunes <arthurnunescarvalho2@gmail.com>
2025-02-21 20:11:15 -0300

    primeiro comit

---
+++ os/main.py
@@ -0,0 +1,42 @@
+#importação de biblioetecas 
+import random
+import time
+import os 
+
+#uma introdução do programa
+print('seja bem vindo ao gerado de dados')
+
+#um tempinho para não ir tudo instanio 
+time.sleep(2)#2seconds
+
+#parte em loop
+while True:
+    print('')#só para dar um espaço 
+    #atribir um valor aleatori entre 1 á 6 a variavel n
+    n = random.randrange(1, 6) 
+
+    #informa para o usario qual foi o n° gerado
+    print(f'O valor do dado é:  {n}')
+    print("")
+
+    #questona se o usario quer continuar com o programar
+    input_user = input("vc que rodar o dado denovo? Responda com Y = sim e N = não.")
+
+    #caso a reposda seja n(não) então sera finlizado o programa
+    if input_user == 'n' or  input_user == 'N':
+        print('ok até mais')
+        break  
+
+    #caso ele seja y(sim) então o programa entrar em loop
+    elif input_user == 'y' or input_user == 'Y':
+        #limpar o terminal
+        os.system("cls")
+        time.sleep(3)#3secondsc
+        pass
+
+    #caso não seja inserido n/y o programa ira interpleta como sim além de dar um saida de erro para o usario
+    else: 
+        print('caracterio invalido')
+        print('o programa dara outro n° a você.')
+        time.sleep(3)#3seconds
+        
---
+++ sistema_de_login/main.py
@@ -0,0 +1,15 @@
+yAndN = input(f/"vc já tem conta?" "reponda com y= para sim, e com n = para não") =  str
+def ferificar_login():
+    if yAndN == "y" or yAndN == "Y":
+        ferificar_senha()
+
+def ferificar_senha():
+    pass
+
+
+
+
+
+
+
+
