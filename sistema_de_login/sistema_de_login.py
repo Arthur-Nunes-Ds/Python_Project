f7095327b104239e9756ebd7d1c8fcfa23b4d043
nunes <arthurnunescarcalho2@gmail.com>
2025-03-01 21:21:07 -0300

    sistema de login autalizado 0.1

--- sistema_de_login/sistema_de_login.py
+++ sistema_de_login/sistema_de_login.py
@@ -8,6 +8,8 @@ with open("sistema_de_login/dados.json", "r", encoding= "utf-8") as arquivo_lido
         logins_amarzernes = dict(json.load(arquivo_lidor))
 
 def cria_conta():
+    print("criar conta")
+
     #vars já pedindos o input do usario
     user_login = input("digite um login: ")
     user_senha = input("digite uma senha: ")
@@ -22,7 +24,7 @@ def cria_conta():
         #ver se o "input1" = "y" ou = 'n'
         if input1 == "Y" or "y":
             #se for sim ele chama a parte de login
-            login_conta()
+            login_conta(0)
         elif input1 == "n" or "N":
             #se for não ele chama a parte de criar
             print('escolha outro "login" ')
@@ -34,39 +36,56 @@ def cria_conta():
             json.dump(logins_amarzernes, escrever, indent=2)
         #limpa o terminal e chama a parte de login
         os.system("cls")
-        login_conta()
+        print("Login")
+        login_conta(0)
             
-
-def login_conta():
-    print("Login")
+def login_conta(erro:int):
+    #vars de input/processo autualizado
     user_login = input("Usuario: ")
     user_senha = input("Senha: ")
     user_correct = False
     senha_correct = False
-    
 
+    #verefica se user esite
     if user_login in logins_amarzernes.keys():
         user_correct = True
     
+    #verifica a senha do user
     if user_senha in logins_amarzernes[user_login]: 
         senha_correct = True
     
+    #verefica o user/senha são verdairas
     if user_correct and senha_correct:
         print("acesso liberado")
+        #finaliza o programa
         EncodingWarning()
     else:
         print("login/senha incorretas")
-        incio_do_programa()
+        #verefica as tentavias do usario depois de 5 ele finaliza o programa sem  
+        #libera para usario
+        if erro != 5:
+            print(f"Tentativas: {erro}")
+            #add mas um ponto no erro
+            login_conta(erro + 1)
+        if erro == 6:
+            print("muitas tentitas seguidas") 
+            EncodingWarning()
 
 def  incio_do_programa():
     print("Seja Bem vindo")
     ja_posui_conta = input("vc já tem uma conta? Responda com Y para sim e N para não: ")
     if ja_posui_conta == 'y' or ja_posui_conta == 'Y':
-        login_conta()
+        time.sleep(2)
+        os.system("cls")
+        login_conta(0)
     elif ja_posui_conta == 'n' or ja_posui_conta == "N":
+        time.sleep(2)
+        os.system("cls")
         cria_conta()
     else:
         print("caracter invalido")
+        time.sleep(2)
+        os.system("cls")
         incio_do_programa()
 
-incio_do_programa()
+#incio_do_programa()
