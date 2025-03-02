3c1840bfe278e0c15bd7f8bbc634b7bd88c8fae5
nunes <arthurnunescarcalho2@gmail.com>
2025-03-02 04:37:23 -0300

    comentado em ingles e em ptbr

--- sistema_de_login/sistema_de_login.py
+++ sistema_de_login/sistema_de_login.py
@@ -25,18 +25,20 @@ with open("sistema_de_login/dados.json", "r", encoding= "utf-8") as arquivo_lido
         logins_amarzernes = dict(json.load(arquivo_lidor))
 
 def cria_conta():
-    print("criar conta")
+    print("===========================")
+    print("criar conta/create account")
+    print("===========================")
 
     #vars já pedindos so input's do usuário/vars already asking for user input
-    user_login = input("digite um login: ")
-    user_senha = input("digite uma senha: ")
+    user_login = input("digite um login/enter a login: ")
+    user_senha = input("digite uma senha/enter a password: ")
     
     #verificar se o login foi inserido estiver na var "externa"/check if the login entered is in the "external" var
     if user_login in logins_amarzernes:
         #se o usuário ele executura os seguintes passos/if the user who performs the following steps
 
-        print('o "login" já esite')
-        input1 = input('o "login" já esite /n deja loga-lo ? Responda com Y para sim e N para não:')
+        print('o "login" já existe \n "login" already exists')
+        input1 = input('o "login" já exite deja loga-lo ? Responda com Y para sim e N para não\nDoes the "login" site already allow you to log in? Answer with Y for yes and N for no:')
 
         #ver se o "input1" = "y" ou = 'n'/see if "input1" = "y" ou = 'n'
         if input1 == "Y" or "y":
@@ -46,7 +48,7 @@ def cria_conta():
             login_conta(0)
         elif input1 == "n" or "N":
             #se for não ele chama a parte de criar/If it is not, it calls the create part
-            print('escolha outro "login" ')
+            print('escolha outro "login"\n choose another "login"')
             delay(2)
             clear()
             cria_conta()
@@ -62,10 +64,12 @@ def cria_conta():
         login_conta(0)
             
 def login_conta(erro:int):
+    print("======")
     print("Login")
+    print("======")
     #vars de input ou processo autualizado/updated input or process stick
-    user_login : str = input("Usuario: ") 
-    user_senha : str = input("Senha: ") 
+    user_login : str = input("Usuario/user: ") 
+    user_senha : str = input("Senha/password: ") 
     user_correct = False
     senha_correct = False
 
@@ -81,7 +85,7 @@ def login_conta(erro:int):
     #verificar o user e senha são verdadeiras/ 
     #check the username and password are true
     if user_correct and senha_correct:
-        print("acesso liberado")
+        print("acesso liberado/free access")
         #abre ou executa o arquivo "protegido"opens or executes the "protected" file
         #estrutura, *=diretorio ou o que q for executa:/
         #structure, *=directory or whatever executes:
@@ -91,27 +95,29 @@ def login_conta(erro:int):
         #ends the program
         EncodingWarning()
     else:
-        print("login/senha incorretas")
+        print("login ou senha incorretas/Incorrect login or password")
         #verefica as tentavias do usario depois de 5 ele finaliza o programa sem  
         #libera para usario/
         #check the user's attempts after 5 he ends the program without
         #release to user
         if erro != 5:
-            print(f"Tentativas: {erro}")
+            print(f"Tentativas/attempts: {erro}")
             delay(2)
             clear()
             #add mais um ponto no erro
             #add one more point to the error
             login_conta(erro + 1)
         if erro == 6:
-            print("muitas tentitas seguidas") 
+            print("muitas tentativas seguidas\nmany attempts in a row") 
             EncodingWarning()
 
 def  incio_do_programa():
-    print("Seja Bem vindo")
+    print("======================")
+    print("Seja Bem vindo/welcome")
+    print("======================")
     #var input do user se ele já tem uma conta no sistema 
     #var user input if he already has an account in the system
-    ja_posui_conta = input("vc já tem uma conta? Responda com Y para sim e N para não: ")
+    ja_posui_conta = input("vc já tem uma conta? Responda com Y para sim e N para não\nDo you already have an account? Answer with Y for yes and N for no:")
 
     #verificar a resposta do user/
     #check user response
@@ -133,7 +139,7 @@ def  incio_do_programa():
     #ver ser o carter é valido seguindo o padrão ABNT 2/
     #see if the character is valid following the ABNT 2 standard
     else:
-        print("caracter invalido")
+        print("caracter invalido/invalid character")
         delay(2)
         clear()
         incio_do_programa()
